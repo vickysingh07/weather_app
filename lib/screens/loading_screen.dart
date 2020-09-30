b945d8baa823e53d7204dd106c8309659a4dcd7a
Vicky <vicky1910098@akgec.ac.in>
2020-10-01 02:40:08 +0530

    chapter 11C

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/services/location.dart';
 import 'package:weather_app/services/networking.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'location_screen.dart';
 
 const apiKey = 'a15c440df7e641c3249f1ca92e9a9529';
 
@@ -28,11 +30,17 @@ class _LoadingScreenState extends State<LoadingScreen> {
     NetworkHelper networkHelper = NetworkHelper(
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=longitude&appid=$apiKey');
 
-    var weatherData = await NetworkHelper.getData();
+    var weatherData = await networkHelper.getData();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body:
+          Center(child: SpinKitDoubleBounce(color: Colors.white, size: 100.0)),
+    );
   }
 }
