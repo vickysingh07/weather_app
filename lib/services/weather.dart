33d4a9c6a3ef00baa6a121e2f8f8d0dbb33bdeb4
Vicky <vicky1910098@akgec.ac.in>
2020-10-02 02:08:45 +0530

    chapter 15C

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,6 +5,13 @@ const apiKey = '4384fe44cfa4bdb9c69e568c24bf34b1';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=numeric');
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
     await location.getCurrentLocation();
